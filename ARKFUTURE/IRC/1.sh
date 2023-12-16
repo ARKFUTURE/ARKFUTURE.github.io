c6f941e775df82564fb94e6aab98aa09d7236916
ARKFUTURE <arkfuture@foxmail.com>
2023-12-16 11:11:38 +0800

    0

--- ARKFUTURE/IRC/1.sh
+++ ARKFUTURE/IRC/1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash  
-echo "脚本版本 :0.1 "
+echo "脚本版本 :ubunto"
 echo "下载 inspircd最新版 "
 wget https://github.com/inspircd/inspircd/releases/download/v3.16.1/inspircd_3.16.1.ubuntu20.04.1_amd64.deb
+
