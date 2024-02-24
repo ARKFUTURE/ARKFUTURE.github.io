cf3fa0dbcdb832866a7cd12e8bba953b0ee95d81
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2024-02-24 11:29:56 +0800

    0

--- ARKFUTURE/IRC/build.sh
+++ ARKFUTURE/IRC/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-echo "脚本版本 :9 "
+echo "脚本版本 :11 "
 echo "从源码构建INSPIRCD "
 echo "适用于Debian系统 "
 read -p "请输入要编译INSPIRCD的版本号(如:3.17.0)： " versi
@@ -22,6 +22,7 @@ echo "启用使用第三方依赖的模组 "
 ./configure --enable-extras ssl_openssl
 echo "执行 ./configure 生成make文件 部分操作需要您手动操作"
 echo "如果您在上一步中启用了TLS(SSL)模块,则系统将询问您是否要生成自签名证书以用于测试目的.如果你想这样做，然后输入"yes",按回车键继续,并填写它提示你的细节。否则,请按enter跳过此步骤。"
+./configure
 sleep 4s
 make install
 echo "安装完成,您可继续配置 "
