18be450f910f5c8b19055958b9c400fee41bbfce
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2024-02-24 14:43:01 +0800

    0

--- ARKFUTURE/IRC/1.sh
+++ ARKFUTURE/IRC/1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash  
 echo "脚本版本 :14 "
+echo "Debian"
 service inspircd stop
 systemctl stop inspircd
 sleep 1s
@@ -52,8 +53,52 @@ head -n 1 rules.txt
 head -n 1 .opers.conf 
 head -n 1 .ssl.conf 
 head -n 1 .helpop.conf
-echo "结束脚本  "
-cd ~
+
+
+#证书生成
+read -p "是否生成证书(yes/no): " input
+if [ "$input" = "yes" ]; then
+    echo "开始下载openssl"
+    apt update && apt upgrade -y && apt install openssl
+    mkdir /etc/inspircd/.ca/
+    chmod 777 /etc/inspircd/.ca/ -R
+    cd /etc/inspircd/.ca/
+    clear
+
+    echo "执行证书生成命令"
+    echo "生成私钥 key.pem,部分操作您需要手动执行"
+    openssl genrsa -out key.pem 2048
+    clear
+    echo "生成证书签名请求 CSR,cert.pem,部分操作您需要手动执行"
+    openssl req -new -key key.pem -out csr.pem
+    clear
+    echo "使用自签名证书生成CA证书,授权730天 ca.pem,部分操作您需要手动执行"
+    openssl x509 -req -days 730 -in csr.pem -signkey key.pem -out ca.pem
+    clear
+    echo "生成Diffie-Hellman参数 dhparams.pem,部分操作您需要手动执行"
+    openssl dhparam -out dhparams.pem 2048
+
+    echo "所有证书生成完毕,正在执行剩下的脚本"
+    chmod 644 /etc/inspircd/.ca/ -R
+    echo "结束脚本  "
+    cd ~
+    
+
+
+elif [ "$input" = "no" ]; then
+    echo "结束脚本  "
+    cd ~
+else
+    echo "结束脚本  "
+    cd ~
+fi
+
+
+
+
+
+
+
 
 
 
