a43972b3b3ca56ee7371ec8eaa7ce93b371bed7c
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2024-02-24 10:55:21 +0800

    0

--- ARKFUTURE/IRC/build.sh
+++ ARKFUTURE/IRC/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash  
-echo "脚本版本 :7 "
+echo "脚本版本 :9 "
 echo "从源码构建INSPIRCD "
 echo "适用于Debian系统 "
 read -p "请输入要编译INSPIRCD的版本号(如:3.17.0)： " versi
 echo "更新软件包 "
 apt update && apt upgrade -y
 echo "安装依赖 "
-apt install git build-essential openssl gnutls-bin -y
+apt install git build-essential autoconf openssl gnutls-bin -y
 apt clean
 echo "cd到~ "
 cd ~
@@ -20,7 +20,7 @@ cd ./inspircd
 echo "启用使用第三方依赖的模组 "
 # 启用OpenSsl
 ./configure --enable-extras ssl_openssl
-echo "开始编译安装 "
+echo "执行 ./configure 生成make文件 部分操作需要您手动操作"
 echo "如果您在上一步中启用了TLS(SSL)模块,则系统将询问您是否要生成自签名证书以用于测试目的.如果你想这样做，然后输入"yes",按回车键继续,并填写它提示你的细节。否则,请按enter跳过此步骤。"
 sleep 4s
 make install
