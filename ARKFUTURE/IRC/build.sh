2fb2f4f1bc960bd1446b78f9d7c3249c8f9b1cd3
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2024-02-24 11:34:54 +0800

    0

--- ARKFUTURE/IRC/build.sh
+++ ARKFUTURE/IRC/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash  
-echo "脚本版本 :11 "
+echo "脚本版本 :13 "
 echo "从源码构建INSPIRCD "
-echo "适用于Debian系统 "
-read -p "请输入要编译INSPIRCD的版本号(如:3.17.0)： " versi
+echo "适用于Debian系统 INSPIRCD版本为 [v3.17.0]"
+sleep 5s
+clear
 echo "更新软件包 "
 apt update && apt upgrade -y
 echo "安装依赖 "
@@ -11,10 +12,10 @@ apt clean
 echo "cd到~ "
 cd ~
 echo "获取源代码 解压 删除下载文件"
-wget https://github.com/inspircd/inspircd/archive/refs/tags/v$versi.tar.gz
-tar -xvf "v$versi.tar.gz"
-rm -rf v$versi.tar.gz
-mv inspircd-$versi inspircd
+wget https://github.com/inspircd/inspircd/archive/refs/tags/v3.17.0.tar.gz
+tar -xvf "v3.17.0.tar.gz"
+rm -rf v3.17.0.tar.gz
+mv inspircd-3.17.0 inspircd
 chmod 777 ./inspircd/ -R
 cd ./inspircd
 echo "启用使用第三方依赖的模组 "
@@ -23,10 +24,9 @@ echo "启用使用第三方依赖的模组 "
 echo "执行 ./configure 生成make文件 部分操作需要您手动操作"
 echo "如果您在上一步中启用了TLS(SSL)模块,则系统将询问您是否要生成自签名证书以用于测试目的.如果你想这样做，然后输入"yes",按回车键继续,并填写它提示你的细节。否则,请按enter跳过此步骤。"
 ./configure
-sleep 4s
+sleep 5s
 make install
 echo "安装完成,您可继续配置 "
-clear
 echo "若要卸载,在源代码的构建目录(通常是build目录)下执行make uninstall命令 "
 
 
