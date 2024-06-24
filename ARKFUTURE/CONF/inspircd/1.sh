2eea6599ee33d95f621e351cdbc6676608ec5563
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2024-06-24 20:40:00 +0800

    0

--- ARKFUTURE/CONF/inspircd/1.sh
+++ ARKFUTURE/CONF/inspircd/1.sh
@@ -1,25 +1,32 @@
 timestamp=$(date +%s)  
-apt update && apt upgrade && apt install curl
+apt update && apt upgrade && apt install wget
 cd /etc/inspircd
 rm -rf *
 echo "下载配置文件1"
-curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/inspircd.conf -o inspircd.conf
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/inspircd.conf
 mkdir conf
 cd /etc/inspircd/conf
 echo "下载配置文件2"
-curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/helpop.conf -o helpop.conf
-curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/opers.conf -o opers.conf
-curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/permchannels.conf -o permchannels.conf
-curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/ssl.conf -o ssl.conf
-curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/xline.db -o xline.db
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/helpop.conf
+echo "下载配置文件2"
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/opers.conf
+echo "下载配置文件2"
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/permchannels.conf
+echo "下载配置文件2"
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/ssl.conf
+echo "下载配置文件2"
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/xline.db
 cd /etc/inspircd
 mkdir txt
 cd /etc/inspircd/txt
 echo "下载配置文件3"
-curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/motd.txt -o motd.txt
-curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/opermotd.txt -o opermotd.txt
-curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/quotes.txt -o quotes.txt
-curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/rules.txt -o rules.txt
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/motd.txt
+echo "下载配置文件3"
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/opermotd.txt
+echo "下载配置文件3"
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/quotes.txt
+echo "下载配置文件3"
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/rules.txt
 echo "下载完成"
 cd /etc/inspircd/
 echo "$timestamp" >> updatetime
