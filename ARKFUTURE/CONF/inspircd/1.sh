a96e342672186c6cd8b2c194b96aace3c29d6712
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2024-06-24 20:55:39 +0800

    0

--- ARKFUTURE/CONF/inspircd/1.sh
+++ ARKFUTURE/CONF/inspircd/1.sh
@@ -18,6 +18,6 @@ wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/quotes.txt
 wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/rules.txt
 cd /etc/inspircd/
 echo "$timestamp" >> updatetime
-chmod 644 -R /etc/inspircd/*
-chmod 644 -R /etc/inspircd/
+chmod -x -R /etc/inspircd/*
+chmod -x -R /etc/inspircd/
 exit
