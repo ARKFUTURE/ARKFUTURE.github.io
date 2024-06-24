fde34f6a7b0b54e6a9698440c80918467919165a
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2024-06-24 20:26:44 +0800

    0

--- ARKFUTURE/CONF/inspircd/1.sh
+++ ARKFUTURE/CONF/inspircd/1.sh
@@ -3,23 +3,23 @@ apt update && apt upgrade && apt install curl
 cd /etc/inspircd
 rm -rf *
 echo "下载配置文件1"
-curl https://arkfuture.github.io/CONF/inspircd/inspircd.conf -o inspircd.conf
+curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/inspircd.conf -o inspircd.conf
 mkdir conf
 cd /etc/inspircd/conf
 echo "下载配置文件2"
-curl https://arkfuture.github.io/CONF/inspircd/conf/helpop.conf -o helpop.conf
-curl https://arkfuture.github.io/CONF/inspircd/conf/opers.conf -o opers.conf
-curl https://arkfuture.github.io/CONF/inspircd/conf/permchannels.conf -o permchannels.conf
-curl https://arkfuture.github.io/CONF/inspircd/conf/ssl.conf -o ssl.conf
-curl https://arkfuture.github.io/CONF/inspircd/conf/xline.db -o xline.db
+curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/helpop.conf -o helpop.conf
+curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/opers.conf -o opers.conf
+curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/permchannels.conf -o permchannels.conf
+curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/ssl.conf -o ssl.conf
+curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/xline.db -o xline.db
 cd /etc/inspircd
 mkdir txt
 cd /etc/inspircd/txt
 echo "下载配置文件3"
-curl https://arkfuture.github.io/CONF/inspircd/txt/motd.txt -o motd.txt
-curl https://arkfuture.github.io/CONF/inspircd/txt/opermotd.txt -o opermotd.txt
-curl https://arkfuture.github.io/CONF/inspircd/txt/quotes.txt -o quotes.txt
-curl https://arkfuture.github.io/CONF/inspircd/txt/rules.txt -o rules.txt
+curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/motd.txt -o motd.txt
+curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/opermotd.txt -o opermotd.txt
+curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/quotes.txt -o quotes.txt
+curl https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/rules.txt -o rules.txt
 echo "下载完成"
 cd /etc/inspircd/
 echo "$timestamp" >> updatetime
