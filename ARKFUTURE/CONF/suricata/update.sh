6f429a38cd435497bdfb94e008170065164adc94
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2024-06-30 12:20:36 +0800

    0

--- ARKFUTURE/CONF/suricata/update.sh
+++ ARKFUTURE/CONF/suricata/update.sh
@@ -1,5 +1,9 @@
 echo "需要ROOT权限执行本脚本"
 echo "此脚本为Debian系统安装Suricata后的升级操作"
+apt install wget
+wget https://arkfuture.github.io/CONF/suricata/suricata.yaml
+mv -f suricata.yaml /etc/suricata/suricata.yaml
+chmod 644 /etc/suricata/suricata.yaml
 suricata-update
 cd /etc/suricata/suricata.yaml
 cp /var/lib/suricata/rules/suricata.rules /etc/suricata/rules/suricata.rules
