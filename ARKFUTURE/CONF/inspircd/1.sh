4fb7115fbc32dada0d87807c72a001ea6460160f
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2024-07-10 15:22:28 +0800

    0

--- ARKFUTURE/CONF/inspircd/1.sh
+++ ARKFUTURE/CONF/inspircd/1.sh
@@ -1,21 +1,3 @@
-timestamp=$(date +%s)  
-apt update && apt upgrade && apt install wget
-cd /etc/inspircd
-rm -rf *
-mkdir conf
-mkdir txt
-wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/inspircd.conf
-cd /etc/inspircd/conf
-wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/helpop.conf
-wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/opers.conf
-wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/permchannels.conf
-wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/ssl.conf
-wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/xline.db
-cd /etc/inspircd/txt
-wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/motd.txt
-wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/opermotd.txt
-wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/quotes.txt
-wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/rules.txt
-cd /etc/inspircd/
-echo "$timestamp" >> updatetime
-chmod 644 /etc/inspircd/ -R
+wget https://mirror.ghproxy.com/https://github.com/inspircd/inspircd/releases/download/v4.0.1/inspircd_4.0.1.deb12u1_amd64.deb
+wget https://mirror.ghproxy.com/https://github.com/fatedier/frp/releases/download/v0.59.0/frp_0.59.0_linux_amd64.tar.gz
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/1.sh
---
+++ ARKFUTURE/CONF/inspircd/2.sh
@@ -0,0 +1,21 @@
+timestamp=$(date +%s)  
+apt update && apt upgrade && apt install wget
+cd /etc/inspircd
+rm -rf *
+mkdir conf
+mkdir txt
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/inspircd.conf
+cd /etc/inspircd/conf
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/helpop.conf
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/opers.conf
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/permchannels.conf
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/ssl.conf
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/conf/xline.db
+cd /etc/inspircd/txt
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/motd.txt
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/opermotd.txt
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/quotes.txt
+wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/txt/rules.txt
+cd /etc/inspircd/
+echo "$timestamp" >> updatetime
+chmod 644 /etc/inspircd/ -R
