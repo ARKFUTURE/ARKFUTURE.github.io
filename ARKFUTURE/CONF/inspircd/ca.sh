f731e5e2bd0c88d6f2b9b5cb562454956257bcef
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2024-07-12 16:16:50 +0800

    0

--- ARKFUTURE/CONF/inspircd/ca.sh
+++ ARKFUTURE/CONF/inspircd/ca.sh
@@ -4,13 +4,13 @@
 openssl genpkey -algorithm RSA -out ca.key.pem -pkeyopt rsa_keygen_bits:2048  
   
 # 使用私钥生成CA证书（自签名）  
-openssl req -new -x509 -key ca.key.pem -days 365 -out ca.pem -subj "/C=CN/ST=Beijing/L=Beijing/O=Example/OU=IT/CN=Example Root CA"  
+openssl req -new -x509 -key ca.key.pem -days 365 -out ca.pem -subj "/C=HK/ST=Beijing/L=Beijing/O=Example/OU=IT/CN=Example Root CA"  
   
 # 生成服务器私钥  
 openssl genpkey -algorithm RSA -out key.pem -pkeyopt rsa_keygen_bits:2048  
   
 # 生成证书签名请求（CSR）  
-openssl req -new -key key.pem -out csr.pem -subj "/C=CN/ST=Beijing/L=Beijing/O=Example/OU=IT/CN=example.com"  
+openssl req -new -key key.pem -out csr.pem -subj "/C=HK/ST=Kowloon/L=Kowloon/O=ARKFUTURE/OU=SOC/CN=域名"  
   
 # 生成Diffie-Hellman参数  
 openssl dhparam -out dhparams.pem 2048  
