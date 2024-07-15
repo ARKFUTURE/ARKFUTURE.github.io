e9d8e87e4aed74d676289096782dfc3ed41d8eff
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2024-07-15 20:40:29 +0800

    0

--- ARKFUTURE/CONF/inspircd/1.sh
+++ ARKFUTURE/CONF/inspircd/1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
-wget https://mirror.ghproxy.com/https://github.com/inspircd/inspircd/releases/download/v4.0.1/inspircd_4.0.1.deb12u1_amd64.deb
+wget https://mirror.ghproxy.com/https://github.com/inspircd/inspircd/releases/download/v4.1.0/inspircd_4.1.0.deb12u1_amd64.deb
 wget https://mirror.ghproxy.com/https://github.com/fatedier/frp/releases/download/v0.59.0/frp_0.59.0_linux_amd64.tar.gz
 wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/1.sh
