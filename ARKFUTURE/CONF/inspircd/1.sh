ac3b4f7020781febcb25bb6266ed355a3ba033ff
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2024-07-22 13:21:26 +0800

    0

--- ARKFUTURE/CONF/inspircd/1.sh
+++ ARKFUTURE/CONF/inspircd/1.sh
@@ -4,4 +4,4 @@ apt update && apt upgrade && apt install wget build-essential cmake -y
 wget https://mirror.ghproxy.com/https://github.com/inspircd/inspircd/releases/download/v4.1.0/inspircd_4.1.0.deb12u1_amd64.deb
 wget https://mirror.ghproxy.com/https://github.com/anope/anope/archive/refs/tags/2.1.7.zip
 wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/2.sh
-wget https://arkfuture.github.io/ARKFUTURE/CONF/anope/2.sh
+wget https://arkfuture.github.io/ARKFUTURE/CONF/anope/anope.zip
