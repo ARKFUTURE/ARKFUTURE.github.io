6d9123cec3ae56916603a954e9ac9163fea06af6
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2024-07-22 11:41:54 +0800

    0

--- ARKFUTURE/CONF/inspircd/1.sh
+++ ARKFUTURE/CONF/inspircd/1.sh
@@ -3,3 +3,4 @@
 wget https://github.com/inspircd/inspircd/releases/download/v4.1.0/inspircd_4.1.0.deb12u1_amd64.deb
 wget https://github.com/anope/anope/archive/refs/tags/2.1.7.zip
 wget https://arkfuture.github.io/ARKFUTURE/CONF/inspircd/2.sh
+wget https://arkfuture.github.io/ARKFUTURE/CONF/anope/2.sh
