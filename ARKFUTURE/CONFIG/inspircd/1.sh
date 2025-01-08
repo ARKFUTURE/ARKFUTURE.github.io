9a66ab4775d49c2953cbae7703d819723967bb78
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-01-08 19:08:50 +0800

    update

--- ARKFUTURE/CONFIG/inspircd/1.sh
+++ ARKFUTURE/CONFIG/inspircd/1.sh
@@ -1,5 +1,5 @@
 echo "inspircd 初始化 (安装+配置), 仅限第一次安装, 请手动启动和自行修改配置"
-apt update && apt upgrade && apt install wget build-essential cmake -y
+apt update && apt upgrade && apt install wget build-essential-y
 wget https://mirror.ghproxy.com/https://github.com/inspircd/inspircd/releases/download/v4.5.0/inspircd_4.5.0.deb12u1_amd64.deb
 chmod 777 inspircd_4.5.0.deb12u1_amd64.deb
 apt install ./inspircd_4.5.0.deb12u1_amd64.deb -y
