7c1cfb7cb4278446acfaabee4968732537b30284
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-01-10 18:33:37 +0800

    update

--- ARKFUTURE/CONFIG/inspircd/make.sh
+++ ARKFUTURE/CONFIG/inspircd/make.sh
@@ -14,7 +14,7 @@ cd inspircd
 ./configure --enable-extras ssl_openssl,ssl_gnutls,argon2,ldap,regex_pcre2,libre2-dev
 ./configure --system
 sleep 3
-make install -j6
+make install -j9
 sleep 3
 echo "编译安装完成,开始下载初始配置到/etc/inspircd/"
 cd /etc/inspircd
