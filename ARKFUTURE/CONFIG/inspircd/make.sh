907425b3caa104115534afccd085b72a10ef2589
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-01-10 10:22:09 +0800

    update

--- ARKFUTURE/CONFIG/inspircd/make.sh
+++ ARKFUTURE/CONFIG/inspircd/make.sh
@@ -5,13 +5,13 @@ echo "现在是root用户权限"
 sleep 3
 echo "开始执行编译脚本"
 apt-get update
-apt-get install -y build-essential wget libssl-dev libpq-dev pkg-config libargon2-dev libmaxminddb-dev libldap2-dev
+apt-get install -y build-essential wget libssl-dev libpq-dev pkg-config libargon2-dev libldap2-dev libpcre2-dev libre2-dev
 wget https://github.com/inspircd/inspircd/archive/refs/tags/v4.5.0.tar.gz
 tar -xzf v4.5.0.tar.gz 
 rm -rf v4.5.0.tar.gz
 mv inspircd-4.5.0 inspircd
 cd inspircd
-./configure --enable-extras ssl_openssl,ssl_gnutls,argon2,geo_maxmind,ldap
+./configure --enable-extras ssl_openssl,ssl_gnutls,argon2,ldap,regex_pcre2,libre2-dev
 ./configure --system
 sleep 3
 make install -j6
