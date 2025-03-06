3b1558e89268b514cb5b004d89b0fe9a2b759d36
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-03-06 23:16:32 +0800

    更新inspircd配置文件，将“插件”更改为“模块”，并更新安装脚本以使用新版本4.6.0

--- ARKFUTURE/CONFIG/inspircd/make.sh
+++ ARKFUTURE/CONFIG/inspircd/make.sh
@@ -7,9 +7,9 @@ echo "开始执行编译脚本"
 apt-get update
 apt-get install -y build-essential wget libssl-dev libpq-dev pkg-config libargon2-dev libldap2-dev libpcre2-dev libre2-dev gnutls-bin libgnutls28-dev
 wget https://github.com/inspircd/inspircd/archive/refs/tags/v4.6.0.tar.gz
-tar -xzf v4.5.0.tar.gz 
-rm -rf v4.5.0.tar.gz
-mv inspircd-4.5.0 inspircd
+tar -xzf v4.6.0.tar.gz 
+rm -rf v4.6.0.tar.gz
+mv inspircd-4.6.0 inspircd
 cd inspircd
 ./configure --enable-extras ssl_openssl,ssl_gnutls,argon2,ldap,regex_pcre2,regex_re2
 ./configure --system
