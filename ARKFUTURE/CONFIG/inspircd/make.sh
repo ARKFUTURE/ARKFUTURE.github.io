177bfc1537377d60d3a98cb030f9caadafe81e8a
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-03-10 15:44:09 +0800

    在make.sh中添加checkinstall以便于软件包管理，注释掉checkinstall命令以避免默认安装

--- ARKFUTURE/CONFIG/inspircd/make.sh
+++ ARKFUTURE/CONFIG/inspircd/make.sh
@@ -5,7 +5,7 @@ echo "现在是root用户权限"
 sleep 3
 echo "开始执行编译脚本"
 apt-get update
-apt-get install -y build-essential wget libargon2-dev libmaxminddb-dev libpsl-dev libldap2-dev libpq-dev libsqlite3-dev libpcre2-dev libre2-dev libgnutls28-dev libssl-dev pkg-config
+apt-get install -y build-essential checkinstall wget libargon2-dev libmaxminddb-dev libpsl-dev libldap2-dev libpq-dev libsqlite3-dev libpcre2-dev libre2-dev libgnutls28-dev libssl-dev pkg-config
 wget https://ghfast.top/https://github.com/inspircd/inspircd/archive/refs/tags/v4.6.0.tar.gz
 tar -xzf v4.6.0.tar.gz 
 rm -rf v4.6.0.tar.gz
@@ -19,6 +19,7 @@ echo "启用了以下插件:"
 ./configure --list
 sleep 3
 make install -j5
+# checkinstall -D --default --pkgname="inspircd" --pkgversion="4.6.0"
 sleep 3
 cd ..
 systemctl daemon-reload
