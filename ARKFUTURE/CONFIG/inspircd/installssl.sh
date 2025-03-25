392517f75e94ce903ee4992dceb0e416eba50b02
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-03-25 11:23:57 +0800

    新增inspircd安装脚本，支持SSL并提供详细安装说明；更新主页链接以反映新脚本

--- ARKFUTURE/CONFIG/inspircd/installssl.sh
+++ ARKFUTURE/CONFIG/inspircd/installssl.sh
@@ -6,18 +6,18 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 echo "现在是root用户权限"
 echo "开始执行安装脚本"
-echo "请注意此软件包是官方软件包"
+echo "此软件包是编译了第三方插件的软件包,包括了不兼容许可的插件,拥有SSL支持,且使用了ssl相关的插件"
+echo "请注意此软件包不是官方软件包,请自行选择是否安装"
 apt update && apt upgrade && apt install -y wget
-wget https://ghfast.top/https://github.com/inspircd/inspircd/releases/download/v4.6.0/inspircd_4.6.0.deb12u1_amd64.deb
+wget https://ghfast.top/https://github.com/ARKFUTURE/ARKFUTURE.github.io/releases/download/20250310/inspircd_4.6.0.deb12u1_amd64.deb
 chmod 777 ./inspircd_4.6.0.deb12u1_amd64.deb
 apt install ./inspircd_4.6.0.deb12u1_amd64.deb -y
 rm -rf ./inspircd_4.6.0.deb12u1_amd64.deb
 echo "安装完成,开始下载初始配置到/etc/inspircd/"
 cd ~
-wget https://arkfuture.github.io/ARKFUTURE/CONFIG/inspircd/config.sh
+wget https://arkfuture.github.io/ARKFUTURE/CONFIG/inspircd/usessl/config.sh
 chmod 777 ./config.sh
 ./config.sh
-echo "脚本运行完成 请修进入/etc/inspircd改为您自己的配置文件"
-sleep 3
 echo "若网络不好请手动重新执行./config.sh脚本 他会替换掉配置文件"
+echo "脚本运行完成 请修进入/etc/inspircd改为您自己的配置文件"
 cd ~
