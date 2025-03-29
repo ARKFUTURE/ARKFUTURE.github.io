eb79bfe66755e583d171f49a0a9b7f546fa409e6
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-03-29 13:13:45 +0800

    更新afeconf.sh中的InspIRCd下载链接，以使用新的镜像地址

--- ARKFUTURE/CONFIG/inspircd/install/afeconf.sh
+++ ARKFUTURE/CONFIG/inspircd/install/afeconf.sh
@@ -11,7 +11,7 @@ echo "此软件包是编译了第三方插件的软件包,包括了不兼容许�
 echo "请注意此软件包不是官方软件包,且拥有许可不兼容的第三方插件,请自行选择是否安装"
 sleep 3
 apt update && apt upgrade -y && apt install -y wget
-wget -O inspircd.deb https://github.com/ARKFUTURE/ARKFUTURE.github.io/releases/download/20250310/inspircd_4.7.0.deb12u1_amd64.deb
+wget -O inspircd.deb https://ghfast.top/https://github.com/ARKFUTURE/ARKFUTURE.github.io/releases/download/20250310/inspircd_4.7.0.deb12u1_amd64.deb
 chmod 777 ./inspircd.deb
 apt install ./inspircd.deb -y
 rm -rf ./inspircd.deb
