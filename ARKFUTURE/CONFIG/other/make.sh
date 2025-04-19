4e5764a39b31d485620f406a97a8de46e5425f22
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-04-19 09:27:42 +0800

    修正make.sh中的目录切换命令，确保正确进入inspircd-4.7.0目录

--- ARKFUTURE/CONFIG/other/make.sh
+++ ARKFUTURE/CONFIG/other/make.sh
@@ -12,7 +12,7 @@ apt-get install -y build-essential checkinstall wget libargon2-dev libpsl-dev li
 wget https://ghfast.top/https://github.com/inspircd/inspircd/archive/refs/tags/v4.7.0.tar.gz
 tar -xzf v4.7.0.tar.gz 
 rm -rf v4.7.0.tar.gz
-cd inspircd inspircd-4.7.0
+cd inspircd-4.7.0
 #./configure --enable-extras --disable-extras
 ./configure --system
 ./configure --update
