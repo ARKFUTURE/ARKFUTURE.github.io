ef5e5e0fafab13c8a469102d7c21ebf75730393b
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-04-20 15:48:35 +0800

    更新afeconf.sh脚本，移除镜像变量并修复wget命令，确保配置文件下载正常

--- ARKFUTURE/CONFIG/inspircd/install/afeconf.sh
+++ ARKFUTURE/CONFIG/inspircd/install/afeconf.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-MIRROR="https://ghfast.top/"
 echo "inspircd 安装脚本 需要root权限 Debian"
 if [ "$(id -u)" -ne 0 ]; then
     echo "错误：请使用 sudo 或 root 权限执行本脚本"
     exit 1
 fi
+echo "dns 更新"
+#wget -O- https://raw.hellogithub.com/hosts >> /etc/hosts
 echo "现在是root用户权限"
 echo "开始执行安装脚本"
 echo "此软件包是编译了第三方插件的软件包,包括了不兼容许可的插件,拥有SSL支持,且使用了ssl相关的插件"
@@ -17,7 +18,7 @@ apt install ./inspircd.deb -y
 rm -rf ./inspircd.deb
 echo "安装完成,开始下载初始配置到/etc/inspircd/"
 cd ~
-wget "${MIRROR}https://raw.githubusercontent.com/ARKFUTURE/config/refs/heads/main/inspircd/config.sh"
+wget https://raw.githubusercontent.com/ARKFUTURE/config/refs/heads/main/inspircd/config.sh"
 chmod 777 ./config.sh
 ./config.sh
 echo "脚本运行完成 请修进入/etc/inspircd改为您自己的配置文件"
