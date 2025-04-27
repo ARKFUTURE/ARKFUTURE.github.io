84dd1601cb64d2477dcf4d7c49a27cb6caf44e8a
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-04-27 22:52:31 +0800

    更新配置脚本，修正chmod命令格式以确保权限设置正确

--- ARKFUTURE/CONFIG/inspircd/afeconf/config.sh
+++ ARKFUTURE/CONFIG/inspircd/afeconf/config.sh
@@ -31,7 +31,7 @@ cd /etc/inspircd/
 mkdir ./conf/ssl
 cd /etc/inspircd/
 chown -R irc:irc /etc/inspircd/
-chmod 644 -R /etc/inspircd/
+chmod -R 644 /etc/inspircd/
 sleep 3
 echo "脚本运行完成 请修改为您自己的配置文件 当前ARKFUTURE配置为"
 head -n 1 ./inspircd.conf
