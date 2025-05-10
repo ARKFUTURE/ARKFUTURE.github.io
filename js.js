d80df51e461b7a84e90cb1053868899f1fa48c22
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-05-10 14:28:13 +0800

    UP

--- js.js
+++ js.js
@@ -39,9 +39,17 @@
         document.head.appendChild(link);
     }
 
+    function UA() {
+        if ( /curl|wget|libcurl/i.test(navigator.userAgent)) { 
+            window.location.replace("/ARKFUTURE/MD/index.md");  
+        }
+}
+
+
     // 页面加载时执行
     window.onload = function() {
         loadCSS();
+        UA();
         setInterval(incrementCounter, 1000);
         setInterval(updateTitle, 1500);
     };
