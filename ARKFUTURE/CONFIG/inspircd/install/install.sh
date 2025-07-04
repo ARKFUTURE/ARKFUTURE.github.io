fa5dee52c494883645fa2c8d53a92ba73177226e
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-07-04 11:53:54 +0800

    update

--- ARKFUTURE/CONFIG/inspircd/install/install.sh
+++ ARKFUTURE/CONFIG/inspircd/install/install.sh
@@ -33,14 +33,13 @@ fi
 chmod 777 ./inspircd.deb
 apt install ./inspircd.deb -y
 rm -rf ./inspircd.deb
-echo "安装完成,开始下载初始配置到/etc/inspircd/"
-echo "您安装了 INSPIRCD 全模块版本 此配置需要您自行配置额外的预编译模块"
+echo "安装完成,开始下载ARKFUTURE初始模板配置到 /etc/inspircd/"
 sleep 2
 cd ~
 curl -L -o config.sh https://arkfuture.github.io/ARKFUTURE/CONFIG/inspircd/install/1.sh
 chmod 777 ./config.sh
 ./config.sh
-echo "脚本运行完成 请进入/etc/inspircd改为您自己的配置文件"
+echo "脚本运行完成 请进入/etc/inspircd/ 根据官网教程改为您自己的配置文件"
 sleep 3
 echo "若网络不好请手动重新执行./config.sh脚本 他会替换掉配置文件"
 cd ~
