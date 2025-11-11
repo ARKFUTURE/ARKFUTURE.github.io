22f8b9ccda81e20d9c27b011a92123c9f2974c07
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-11-11 14:26:29 +0800

    update

--- arkfuture/config/other/install/2.sh
+++ arkfuture/config/other/install/2.sh
@@ -1,41 +1,34 @@
 #!/bin/bash
-echo "更新配置文件"
+echo "Anope配置文件覆盖脚本"
 echo "! 此操作会覆盖配置文件 !"
+echo "ARKFUTURE 服务器正式的配置文件, 是ARKFUTURE公共配置的分支配置"
 sleep 2
 if [ "$(id -u)" -ne 0 ]; then
     echo "错误：请使用 sudo 或 root 权限执行本脚本"
     exit 1
 fi
 echo "现在是root用户权限"
+apt install -y anope
 sleep 2
-echo "开始下载配置文件到/etc/inspircd"
-cd /etc/inspircd
+echo "开始下载配置文件到/etc/anope"
+cd /etc/anope
 rm -rf *
-mkdir conf
-mkdir txt
-curl -L -O --anyauth --user ##修改为用户名##:##修改为密码## http://frp.freefrp.net:10240/SERVER/inspircd.conf
-openssl req -x509 -newkey rsa:4096 -sha256 -days 10 -nodes -keyout key.pem -out cert.pem -subj "/CN=irc.arkfuture.test" -addext "extendedKeyUsage = serverAuth"
-cd /etc/inspircd/conf
-curl -L -O --anyauth --user ##修改为用户名##:##修改为密码## http://frp.freefrp.net:10240/SERVER/modules.conf
-curl -L -O --anyauth --user ##修改为用户名##:##修改为密码## http://frp.freefrp.net:10240/SERVER/opers.conf
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/conf/links.conf
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/conf/helpop.conf
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/conf/ascii.conf
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/conf/generic.conf
-curl -L -O --anyauth https://gh-proxy.com/https://github.com/P3TERX/GeoLite.mmdb/releases/download/2025.08.07/GeoLite2-Country.mmdb
-touch filters.conf
-touch xline.db
-cd /etc/inspircd/txt
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/txt/motd.txt
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/txt/opermotd.txt
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/txt/quotes.txt
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/txt/rules.txt
-cd /etc/inspircd/
-chown -R irc:irc /etc/inspircd/
-find /etc/inspircd -type d -exec chmod 755 {} \;
-find /etc/inspircd -type f -exec chmod 640 {} \;
+curl -L -O --anyauth https://ARKFUTURE.github.io/ARKFUTURE/CONFIG/anope/services.conf
+curl -L -O --anyauth https://ARKFUTURE.github.io/ARKFUTURE/CONFIG/anope/services.motd
+curl -L -O --anyauth https://ARKFUTURE.github.io/ARKFUTURE/CONFIG/anope/modules.conf
+curl -L -O --anyauth https://ARKFUTURE.github.io/ARKFUTURE/CONFIG/anope/nickserv.conf
+curl -L -O --anyauth https://ARKFUTURE.github.io/ARKFUTURE/CONFIG/anope/chanserv.conf
+curl -L -O --anyauth https://ARKFUTURE.github.io/ARKFUTURE/CONFIG/anope/operserv.conf
+curl -L -O --anyauth https://ARKFUTURE.github.io/ARKFUTURE/CONFIG/anope/botserv.conf
+curl -L -O --anyauth https://ARKFUTURE.github.io/ARKFUTURE/CONFIG/anope/global.conf
+curl -L -O --anyauth https://ARKFUTURE.github.io/ARKFUTURE/CONFIG/anope/memoserv.conf
+curl -L -O --anyauth https://ARKFUTURE.github.io/ARKFUTURE/CONFIG/anope/hostserv.conf
+cd /etc/anope
+chown root:root /etc/anope
+chmod 755 /etc/anope
+find /etc/anope -type f -exec chown root:irc {} \;
+find /etc/anope -type f -exec chmod 640 {} \;
 sleep 2
-echo "脚本运行完成 请修改为您自己的配置 当前配置为"
-head -n 1 /etc/inspircd/inspircd.conf
+head -n 1 /etc/anope/services.conf
 sleep 2
-echo "inspircd配置文件下载完成"
+echo "anope配置文件下载完成"
--- arkfuture/config/other/install/afeconf.sh
+++ arkfuture/config/other/install/afeconf.sh
@@ -14,8 +14,37 @@ curl -L -o inspircd.deb https://github.com/ARKFUTURE/ARKFUTURE.github.io/release
 chmod 777 ./inspircd.deb
 apt install ./inspircd.deb -y
 rm -rf ./inspircd.deb
+echo "安装完成"
 ###### INSPIRCD配置
-curl -L -o inspircd.sh https://arkfuture.github.io/ARKFUTURE/CONFIG/other/install/2.sh
-chmod 777 ./inspircd.sh
-sleep 1
-echo "安装结束"
+echo "开始下载配置文件到/etc/inspircd"
+cd /etc/inspircd
+rm -rf *
+mkdir conf
+mkdir txt
+curl -L -O --anyauth --user ##修改为用户名##:##修改为密码## http://frp.freefrp.net:10240/SERVER/inspircd.conf
+openssl req -x509 -newkey rsa:4096 -sha256 -days 10 -nodes -keyout key.pem -out cert.pem -subj "/CN=irc.arkfuture.test" -addext "extendedKeyUsage = serverAuth"
+cd /etc/inspircd/conf
+curl -L -O --anyauth --user ##修改为用户名##:##修改为密码## http://frp.freefrp.net:10240/SERVER/modules.conf
+curl -L -O --anyauth --user ##修改为用户名##:##修改为密码## http://frp.freefrp.net:10240/SERVER/opers.conf
+curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/conf/links.conf
+curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/conf/helpop.conf
+curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/conf/ascii.conf
+curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/conf/generic.conf
+curl -L -O --anyauth https://gh-proxy.com/https://github.com/P3TERX/GeoLite.mmdb/releases/download/2025.08.07/GeoLite2-Country.mmdb
+touch filters.conf
+touch xline.db
+cd /etc/inspircd/txt
+curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/txt/motd.txt
+curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/txt/opermotd.txt
+curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/txt/quotes.txt
+curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/txt/rules.txt
+cd /etc/inspircd/
+chown -R irc:irc /etc/inspircd/
+find /etc/inspircd -type d -exec chmod 755 {} \;
+find /etc/inspircd -type f -exec chmod 640 {} \;
+sleep 2
+cd ~
+head -n 1 /etc/inspircd/inspircd.conf
+sleep 2
+###### 说明
+echo "inspircd配置文件下载完成"
