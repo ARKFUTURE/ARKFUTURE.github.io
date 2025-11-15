9cb93b13b4dc2c3ee805f25dfd832f09344789cc
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-11-15 12:57:01 +0800

    update

--- arkfuture/config/other/install/afeconf.sh
+++ arkfuture/config/other/install/afeconf.sh
@@ -30,18 +30,7 @@ else
 echo "未生成任何证书,请稍自行获取证书并修改/usr/share/inspircd/deploy-ssl.sh后执行此移动证书脚本"
 fi
 cd /etc/inspircd/conf
-curl -L -O --anyauth --user ##USERNAME##:##PASSWORD## http://frp.freefrp.net:10240/modules.conf
-curl -L -O --anyauth --user ##USERNAME##:##PASSWORD## http://frp.freefrp.net:10240/opers.conf
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/conf/links.conf
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/conf/helpop.conf
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/conf/ascii.conf
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/conf/generic.conf
 curl -L -O --anyauth https://git.io/GeoLite2-Country.mmdb
-cd /etc/inspircd/txt
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/txt/motd.txt
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/txt/opermotd.txt
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/txt/quotes.txt
-curl -L -O --anyauth https://arkfuture.github.io/arkfuture/config/inspircd/txt/rules.txt
 cd /etc/inspircd/
 chown -R irc:irc /etc/inspircd/
 find /etc/inspircd -type d -exec chmod 755 {} \;
