7e94cd8a55c9fc830532d01d2134e55e30d62d7a
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-11-17 18:23:10 +0800

    update

--- arkfuture/config/other/install/1.sh
+++ arkfuture/config/other/install/1.sh
@@ -13,18 +13,18 @@ cd /etc/inspircd
 rm -rf *
 mkdir conf
 mkdir txt
-curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/inspircd.conf
+curl -L -O https://arkfuture.github.io/ARKFUTURE/CONFIG/inspircd/inspircd.conf
 cd /etc/inspircd/conf
-curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/conf/modules.conf
-curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/conf/helpop.conf
-curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/conf/opers.conf
-curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/conf/ascii.conf
-curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/conf/generic.conf
+curl -L -O https://arkfuture.github.io/ARKFUTURE/CONFIG/inspircd/conf/modules.conf
+curl -L -O https://arkfuture.github.io/ARKFUTURE/CONFIG/inspircd/conf/helpop.conf
+curl -L -O https://arkfuture.github.io/ARKFUTURE/CONFIG/inspircd/conf/opers.conf
+curl -L -O https://arkfuture.github.io/ARKFUTURE/CONFIG/inspircd/conf/ascii.conf
+curl -L -O https://arkfuture.github.io/ARKFUTURE/CONFIG/inspircd/conf/generic.conf
 cd /etc/inspircd/txt
-curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/txt/motd.txt
-curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/txt/opermotd.txt
-curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/txt/quotes.txt
-curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/txt/rules.txt
+curl -L -O https://arkfuture.github.io/ARKFUTURE/CONFIG/inspircd/txt/motd.txt
+curl -L -O https://arkfuture.github.io/ARKFUTURE/CONFIG/inspircd/txt/opermotd.txt
+curl -L -O https://arkfuture.github.io/ARKFUTURE/CONFIG/inspircd/txt/quotes.txt
+curl -L -O https://arkfuture.github.io/ARKFUTURE/CONFIG/inspircd/txt/rules.txt
 cd /etc/inspircd/
 chown -R irc:irc /etc/inspircd/
 find /etc/inspircd -type d -exec chmod 755 {} \;
