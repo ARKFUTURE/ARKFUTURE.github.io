2954a528d4f899511461075a851320db420d9034
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2025-11-17 18:19:48 +0800

    update

--- arkfuture/config/other/install/afeconf.sh
+++ arkfuture/config/other/install/afeconf.sh
@@ -16,11 +16,39 @@ apt install ./inspircd.deb -y
 rm -rf ./inspircd.deb
 echo "安装完成"
 ###### INSPIRCD配置
-curl -L -o conf.sh https://arkfuture.github.io/arkfuture/config/other/install/3.sh
-chmod 777 conf.sh
-./config.sh
-head -n 1 /etc/inspircd/inspircd.conf
+echo "---->>配置导入脚本,仅适于ARKFUTURE配置,进行中"
+cd /etc/inspircd
+rm -rf *
+mkdir conf
+mkdir txt
+curl -L -O --anyauth --user ##USERNAME##:##PASSWORD## http://frp.freefrp.net:10240/inspircd.conf
+curl -L -O --anyauth https://git.io/GeoLite2-Country.mmdb
+read -rp "是否要生成自签名证书？ [y/N] "
+if [[ "${ans,,}" == "y" || "${ans,,}" == "yes" ]]; then
+openssl req -x509 -newkey rsa:4096 -sha256 -days 30 -nodes -keyout key.pem -out cert.pem -subj "/CN=irc.arkfuture.test" -addext "extendedKeyUsage = serverAuth"
+echo "成功生成自签名证书,证书仅用于测试环境"
+else
+echo "未生成任何证书,请稍自行获取证书并修改/usr/share/inspircd/deploy-ssl.sh后执行此移动证书脚本"
+fi
+cd /etc/inspircd/conf
+curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/conf/modules.conf
+curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/conf/helpop.conf
+curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/conf/opers.conf
+curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/conf/ascii.conf
+curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/conf/generic.conf
+cd /etc/inspircd/txt
+curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/txt/motd.txt
+curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/txt/opermotd.txt
+curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/txt/quotes.txt
+curl -L -O https://arkfuture.top/ARKFUTURE/CONFIG/inspircd/txt/rules.txt
+cd /etc/inspircd/
+chown -R irc:irc /etc/inspircd/
+find /etc/inspircd -type d -exec chmod 755 {} \;
+find /etc/inspircd -type f -exec chmod 640 {} \;
+cd ~
+echo "---->>配置导入脚本,仅适于ARKFUTURE配置,进行中"
 ###### 说明
+head -n 1 /etc/inspircd/inspircd.conf
 echo "inspircd配置文件下载完成"
 echo "若您在/etc/inspircd文件夹下没有发现inspircd.conf"
 echo "则您需要执行./config.sh来重新下载配置文件"
