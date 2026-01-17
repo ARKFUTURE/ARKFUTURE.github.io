11591713fbfa0a6ca214cd21690302adb3df2b6a
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2026-01-17 17:38:03 +0800

    update

--- arkfuture/config/other/install/config.sh
+++ arkfuture/config/other/install/config.sh
@@ -1,25 +1,3 @@
-#!/bin/bash
-echo "此脚本仅用于inspircd/anope及arkfuture配置的安装"
-echo "inspircd 安装脚本 需要root权限 Debian"
-if [ "$(id -u)" -ne 0 ]; then
-    echo "错误: 请使用 sudo 或 root 权限执行本脚本"
-    exit 1
-fi
-echo "现在是root用户权限"
-sleep 3
-###### 安装依赖
-apt update && apt install -y wget curl openssl gnutls-bin
-###### 安装inspircd
-curl -L -o inspircd.deb https://github.com/ARKFUTURE/ARKFUTURE.github.io/releases/download/20250310/inspircd_deb12u1_amd64.deb
-chmod 777 ./inspircd.deb
-apt install ./inspircd.deb -y
-rm -rf ./inspircd.deb
-systemctl disable inspircd
-echo "inspircd安装完成"
-###### 安装anope(Local)
-apt install anope
-systemctl disable anope
-echo "anope安装完成"
 ###### INSPIRCD配置
 cd /etc/inspircd
 rm -rf *
@@ -30,7 +8,7 @@ touch cert.pem
 touch key.pem
 read -rp "是否要生成自签名证书？ [y/N] " ans
 if [[ ${ans,,} == @(y|yes) ]]; then
-    openssl req -x509 -newkey rsa:4096 -sha256 -days 360 -nodes -keyout key.pem -out cert.pem -subj "/CN=irc.arkfuture.irc" -addext "extendedKeyUsage = serverAuth"
+    openssl req -x509 -newkey rsa:4096 -sha256 -days 360 -nodes -keyout key.pem -out cert.pem -subj "/CN=irc.arkfuture.test" -addext "extendedKeyUsage = serverAuth"
     echo "成功生成自签名证书,证书仅用于测试环境"
 else
     echo "未生成任何证书,请稍自行获取证书并修改/usr/share/inspircd/deploy-ssl.sh后执行此移动证书脚本"
@@ -76,13 +54,3 @@ find /etc/anope -type f -exec chown root:irc {} \;
 find /etc/anope -type f -exec chmod 640 {} \;
 cd ~
 echo "anope配置文件下载完成,请修改相关配置以便使用"
-###### 说明
-head -n 1 /etc/inspircd/inspircd.conf
-head -n 1 /etc/anope/services.confi
-curl -L -O --anyauth https://ARKFUTURE.github.io/arkfuture/config/other/install/config.sh
-echo "相关配置文件下载完成"
-echo "若您在/etc/inspircd文件夹下没有发现inspircd.conf"
-echo "或者/etc/anope文件夹下没有发现services.conf"
-echo "则您需要执行./config.sh来重新下载配置文件"
-echo "您需要手动执行systemctl enable 来自启动相关服务"
-rm -rf ./afeconf.sh
